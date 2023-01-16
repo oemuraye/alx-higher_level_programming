c0b74e2f8bec5e132802e899f3d2da51694f1d99
Emuraye Pius 0 <76441493+oemuraye@users.noreply.github.com>
2023-01-16 21:50:57 +0100

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,31 +1,38 @@
 #!/usr/bin/python3
 """
-    contains class Square implements class Rectangle
+contains class Square which implements Rectangle.
 """
+
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     """
-        Square implements rectangle
+    class Square which implements rectangle
     """
+
     def __init__(self, size, x=0, y=0, id=None):
         """
-            initialises Square (overrides Rectangle init)
+        Initializes the instance of the class
+        Args:
+            size: size of the square
+            x: x coordinate of the square
+            y: y coordinate of the square
+            id: id of the square
         """
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
         """
-            returns the size of the square
+        getter for size
         """
         return self.width
 
     @size.setter
     def size(self, value):
         """
-            sets the value of size
+        setter for size
         """
         if type(value) != int:
             raise TypeError("width must be an integer")
@@ -35,40 +42,44 @@ class Square(Rectangle):
         self.width = value
         self.height = value
 
-    def update(self, *args, **kwargs):
-        """
-            assigns key/value argument to attributes
-            kwargs is skipped if args is not empty
-            Args:
-                *args -  variable number of no-keyword args
-                **kwargs - variable number of keyworded args
-        """
-        if len(args) == 0:
-            for key, val in kwargs.items():
-                self.__setattr__(key, val)
-            return
-
-        try:
-            self.id = args[0]
-            self.size = args[1]
-            self.x = args[2]
-            self.y = args[3]
-        except IndexError:
-            pass
-
     def __str__(self):
         """
-            Overloading str function
+        returns string representation of the square
         """
-        return "[{}] ({}) {}/{} - {}".format(type(self).__name__,
-                                             self.id, self.x, self.y,
-                                             self.width)
+        return "[Square] ({}) {}/{} - {}".format(self.id,
+                                                 self.x, self.y, self.size)
+
+    def update(self, *args, **kwargs):
+        """
+        assign an argument to each attribute
+        """
+        if args:
+            for i, arg in enumerate(args):
+                if i == 0:
+                    self.id = arg
+                if i == 1:
+                    self.size = arg
+                if i == 2:
+                    self.x = arg
+                if i == 3:
+                    self.y = arg
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "size":
+                    self.size = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
 
     def to_dictionary(self):
         """
-            Returns the dictionary representation of a Square
+        returns dictionary representation of the square
         """
-        return {'id': getattr(self, "id"),
-                'size': getattr(self, "width"),
-                'x': getattr(self, "x"),
-                'y': getattr(self, "y")}
+        return {"id": self.id,
+                "x": self.x,
+                "size": self.size,
+                "y": self.y
+                }
