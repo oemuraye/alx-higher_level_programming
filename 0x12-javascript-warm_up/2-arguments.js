d5aaba3d60839b152bc17a28ca72f770657812cb
Emuraye Pius 0 <76441493+oemuraye@users.noreply.github.com>
2023-03-14 03:34:38 +0100

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
 import { argv } from 'node:process';
 
-const count = argv.length - 2;
-console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
+const argvLength = argv.length - 2;
+if (argvLength <= 0) {
+  console.log('No argument');
+} else if (argvLength === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
